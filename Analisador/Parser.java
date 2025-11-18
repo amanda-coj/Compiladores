60c99ed7aa4061c43c0ae4c4e19ca3eee6973ba9
amanda-coj <amandacaroline.oj@gmail.com>
2025-11-17 22:29:32 -0300

    Incluindo parse() para retorno seguro da expressão

--- Analisador/Parser.java
+++ Analisador/Parser.java
@@ -13,6 +13,14 @@ class Parser {
     this.tokens = tokens;
   }
 
+  Expr parse() {
+  try {
+    return expression();
+  } catch (ParseError error) {
+    return null;
+  }
+}
+
   private Expr expression() {
     return equality();
   }
@@ -88,6 +96,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+        throw error(peek(), "Expect expression.");
+
   }
 
 

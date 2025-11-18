9880850beaca608ca34d2b84c5651154ad6b615b
amanda-coj <amandacaroline.oj@gmail.com>
2025-11-17 22:22:22 -0300

    Adicionando método error() para tratamento de erros no Parser

--- Analisador/Parser.java
+++ Analisador/Parser.java
@@ -130,7 +130,10 @@ class Parser {
     return tokens.get(current - 1);
   }
 
-
+private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
 
 
 

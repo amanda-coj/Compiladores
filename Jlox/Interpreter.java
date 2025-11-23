fe7587930faaeef243908eebae477d9239614032
amanda-coj/ <amandacaroline.oj@gmail.com>
2025-11-23 13:48:51 -0300

    Iniciando implementação do Interpretador

--- Jlox/Interpreter.java
+++ Jlox/Interpreter.java
@@ -1,4 +1,18 @@
 package Jlox;
 
 class Interpreter implements Expr.Visitor<Object> {
+
+ @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
 }

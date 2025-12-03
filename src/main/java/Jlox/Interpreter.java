ef7f24e3ad56639a6771ca884788ca0094cdf40d
amanda-coj/ <amandacaroline.oj@gmail.com>
2025-12-03 00:20:14 -0300

    Adicionar short-circuit para expressões lógicas

--- src/main/java/Jlox/Interpreter.java
+++ src/main/java/Jlox/Interpreter.java
@@ -23,8 +23,20 @@ class Interpreter implements Expr.Visitor<Object>,
         return -(double) right;
       
     }
-    // Unreachable.
     return null;
+  }
+
+    @Override
+  public Object visitLogicalExpr(Expr.Logical expr) {
+    Object left = evaluate(expr.left);
+
+    if (expr.operator.type == TokenType.OR) {
+      if (isTruthy(left)) return left;
+    } else {
+      if (!isTruthy(left)) return left;
+    }
+
+    return evaluate(expr.right);
   }
 
    @Override

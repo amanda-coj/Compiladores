d855e86d8b3bca34e9795fb957bf2bec5d303c80
efraimdiniz <efraimd57@gmail.com>
2025-12-13 08:17:11 -0300

    Native Functions

--- src/main/java/Jlox/Interpreter.java
+++ src/main/java/Jlox/Interpreter.java
@@ -5,7 +5,34 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
-   private Environment environment = new Environment();
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() { return 0; }
+
+      @Override
+      public Object call(Interpreter interpreter,
+                         List<Object> arguments) {
+        return (double)System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() { return "<native fn>"; }
+    });
+  }
+
+  void interpret(List<Stmt> statements) {
+    try {
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
 
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
@@ -233,17 +260,7 @@ class Interpreter implements Expr.Visitor<Object>,
     return function.call(this, arguments);
   }
 
-  void interpret(List<Stmt> statements) {
-    try {
-      for (Stmt statement : statements) {
-        execute(statement);
-      }
-    } catch (RuntimeError error) {
-      Lox.runtimeError(error);
-    }
-  }
-
-
+  
 
 
 

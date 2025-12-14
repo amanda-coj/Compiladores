f22dfabe7720639f7f0939b85f80ae11b3e47d5e
amanda-coj/ <amandacaroline.oj@gmail.com>
2025-12-14 14:54:00 -0300

    Adicionando LoxInsttance

--- src/main/java/Jlox/LoxClass.java
+++ src/main/java/Jlox/LoxClass.java
@@ -15,4 +15,14 @@ class LoxClass implements LoxCallable {
   public String toString() {
     return name;
   }
-}
+@Override
+  public Object call(Interpreter interpreter,
+                     List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+    return 0;
+  }
